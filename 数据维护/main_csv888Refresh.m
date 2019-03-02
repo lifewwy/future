03bfa056cf8dfce41acfc569c67f9a2d381491ae
lifewwy <lifewwy@163.com>
2019-03-02 22:59:51 +0800

    1

--- 数据维护/main_csv888Refresh.m
+++ 数据维护/main_csv888Refresh.m
@@ -10,6 +10,8 @@ futureCode = {'A','C','RB','M','P','J','ZN','Y','L','AG','TA' ...
     ,'SR','AU','FG','MA','RU','AL','OI','RM','CF','I','JM','CU','V','IF'};
 
 h = 0;
+% fc = [];
+% kDataJoinQuant = [];
 for i = 1:length(futureCode)
     csv888Refresh(futureCode{i},h,fc,kDataJoinQuant);
 end
